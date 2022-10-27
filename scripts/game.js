4759ce9772e45fc567ce3840e4ee634a0685200f
Itallo007 <italosantos.oficial2000@gmail.com>
2022-10-27 00:37:08 -0300

    Ajusta o grid de cartas e cria a função que revela as cartas

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,6 @@
 gameSettings = {
-  pars: 10
+  pars: 10,
+  cardsRevealed: []
 }
 
 const createElementWithClass = (tag, className) => {
@@ -12,16 +13,42 @@ const createCard = () => {
   const card = createElementWithClass('div', 'card');
   const front = createElementWithClass('div', 'face front');
   const back = createElementWithClass('div', 'face back');
+  card.addEventListener('click', revealCard);
 
   card.appendChild(front);
   card.appendChild(back);
 
-  card.addEventListener('click', ({target}) => {
-    target.parentNode.classList.toggle('reveal-card');
-    console.log(target.parentNode)
-  });
-
   return card;
+}
+
+const revealCard = ({target}) => {
+
+  if(target.parentNode.className.includes('reveal-card')) {
+    return
+  }
+  
+  target.parentNode.classList.add('reveal-card');
+  gameSettings.cardsRevealed.push(target.parentNode);
+
+  console.log(gameSettings.cardsRevealed)
+  // console.log(target.parentNode.classList)
+  
+
+  if(gameSettings.cardsRevealed.length === 2) {
+    setTimeout(checkMatching, 1000);
+  }
+
+  // console.log(gameSettings.cardsRevealed)
+
+}
+
+const checkMatching = () => {
+  gameSettings.cardsRevealed.map((item) => {
+    console.log(item)
+    item.classList.remove("reveal-card");
+  })
+  gameSettings.cardsRevealed = []
+
 }
 
 const loadGame = () => {
